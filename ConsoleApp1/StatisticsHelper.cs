23bfc63b5bf07ff72d2a334f55ea3f6952677e5b
Demian Kishyk <s34175@pjwstk.edu.pl>
2026-03-18 22:56:12 +0100

    Create StatisticsHelper architecture

--- ConsoleApp1/StatisticsHelper.cs
+++ ConsoleApp1/StatisticsHelper.cs
@@ -1,6 +1,13 @@
 ﻿namespace ConsoleApp1;
 
-public class StatisticsHelper
+public static class StatisticsHelper
 {
+    public static double Mean(IEnumerable<double> data)
+    {
+        if (data == null || !data.Any())
+            throw new ArgumentException("Коллекция пуста");
+
+        return data.Average();
+    }
     
 }
