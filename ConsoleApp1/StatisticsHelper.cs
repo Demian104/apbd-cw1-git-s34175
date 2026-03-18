862134f77c8c5d9a8dc3b23943fa9f7aff14df30
Demian Kishyk <s34175@pjwstk.edu.pl>
2026-03-18 22:58:46 +0100

    Add median logic to our StatiscticHelper class

--- ConsoleApp1/StatisticsHelper.cs
+++ ConsoleApp1/StatisticsHelper.cs
@@ -9,5 +9,17 @@ public static class StatisticsHelper
 
         return data.Average();
     }
-    
+    public static double Median(IEnumerable<double> data)
+    {
+        if (data == null || !data.Any())
+            throw new ArgumentException("Коллекция пуста");
+
+        var sorted = data.OrderBy(x => x).ToList();
+        int count = sorted.Count;
+
+        if (count % 2 == 0)
+            return (sorted[count / 2 - 1] + sorted[count / 2]) / 2.0;
+
+        return sorted[count / 2];
+    }
 }
